db1b0fe041bee1b9a1d83ff079e1924e09b008ac
Alessandro Monte <46656617+Mongisa@users.noreply.github.com>
2022-12-13 15:39:24 +0000

    Fully Working

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # MensaUnisa
-Progetto che permette di ottenere il menù del giorno della mensa UNISA (in formato pdf)
+Progetto che permette di ottenere il link del menù del giorno della mensa UNISA (in formato pdf)
---
+++ index.js
@@ -0,0 +1,67 @@
+const https = require('https');
+const HTMLParser = require('node-html-parser');
+
+const urlADISURC = 'https://www.adisurcampania.it/archivio2_aree-tematiche_0_8.html';
+
+/**
+ * 
+ * @returns {Promise<{type: string, link: string}>}
+ */
+async function getLunch() {
+    return new Promise((resolve, reject) => {
+        https.get(urlADISURC, (res) => {
+            let data = ''
+            res.on('data', (chunk) => {
+                data += chunk
+            })
+            res.on('end', async () => {
+                const root = HTMLParser.parse(data)
+
+                const result = root.querySelectorAll('a').filter((a) => {
+                    return a.text.includes('Pranzo') || a.text.includes('pranzo')
+                }).map((a) => {
+                    return {
+                        type: a.text,
+                        link: a.getAttribute('href')
+                    }
+                })
+
+                resolve(result[0])
+            })
+        }).on('error', (err) => {
+            reject('Error: ' + err.message)
+        })
+    })
+}
+
+/**
+ * @returns {Promise<{type: string, link: string}>}
+ */
+async function getDinner() {
+    return new Promise((resolve, reject) => {
+        https.get(urlADISURC, (res) => {
+            let data = ''
+            res.on('data', (chunk) => {
+                data += chunk
+            })
+            res.on('end', async () => {
+                const root = HTMLParser.parse(data)
+
+                const result = root.querySelectorAll('a').filter((a) => {
+                    return a.text.includes('Cena') || a.text.includes('cena')
+                }).map((a) => {
+                    return {
+                        type: a.text,
+                        link: a.getAttribute('href')
+                    }
+                })
+                
+                resolve(result[0])
+            })
+        }).on('error', (err) => {
+            reject('Error: ' + err.message)
+        })
+    })
+}
+
+module.exports = { getLunch, getDinner }
